5387a6598c54f83d314205a1b8cde9096491b719
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-26 15:02:54 +0530

    Integrate speech recognition and display messages in the chat interface

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import speech_recognition as sr
+import eel
 
 
 def speak(text):
@@ -11,21 +12,27 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+@eel.expose
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
+        eel.DisplayMessage("Listening...")
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, 10 , 6)
     try:
         print("Recognizing...")
+        eel.DisplayMessage("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}")
+        eel.DisplayMessage(query)
+        speak(query)
+        eel.ShowHood()
     except Exception as e:
         return ""
     return query.lower()
 
-text = takecommand()
+#text = takecommand()
 
-speak(text)
+#speak(text)
