c5d490f6b9e1f45fa98aa8b8f8349e43fca4743f
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-07-03 15:49:32 +0530

    Create database schema for command handling and add YouTube web command

---
+++ engine/db.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('vion.db')
+cursor = conn.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+cursor.execute(query)
+
+# query = "INSERT INTO sys_command VALUES (null, 'One Note', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+# cursor.execute(query)
+# conn.commit()
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (null, 'Youtube', 'https://www.youtube.com/')"
+cursor.execute(query)
+conn.commit()
+
+
+
+
+
+
+# cursor.execute("DELETE FROM sys_command WHERE id = ?", (4,))
+
+# conn.commit()
+# conn.close()
